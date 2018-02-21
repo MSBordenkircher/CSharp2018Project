c2e9a633194b85b56037ee94084f091608eba6a0
Michael Bordenkircher <michaelsbordenkircher@gmail.com>
2018-02-20 20:00:55 -0500

    First Commit of project. Initiated project as MVC web app.

---
+++ CSharp2018Project/CSharp2018Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharp2018Project.MvcApplication" Language="C#" %>
---
+++ CSharp2018Project/CSharp2018Project/Models/Drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharp2018Project.Models
+{
+    public class Drink
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+       public string Category { get; set; }
+       public int Score { get; set; }
+    }
+}
