e71d563ad747a1a80441ab6f6ed5160bf37099bf
Michael Bordenkircher <michaelsbordenkircher@gmail.com>
2018-02-27 19:00:45 -0500

    Added Database

---
+++ CSharp2018Project/CSharp2018Project/Models/CSharp2018ProjectContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CSharp2018Project.Models
+{
+    public class CSharp2018ProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CSharp2018ProjectContext() : base("name=CSharp2018ProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CSharp2018Project.Models.Drink> Drinks { get; set; }
+    }
+}
--- CSharp2018Project/CSharp2018Project/Models/Drink.cs
+++ CSharp2018Project/CSharp2018Project/Models/Drink.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace CSharp2018Project.Models
 {
@@ -10,6 +11,8 @@ namespace CSharp2018Project.Models
        public int Id { get; set; }
        public string Name { get; set; }
        public string Category { get; set; }
-       public int Score { get; set; }
+       public int? Score { get; set; }
     }
+
+
 }
