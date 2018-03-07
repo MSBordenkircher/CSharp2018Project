d4bb1e13380cd024dfd848c442e7c385cc9c6ace
Michael Bordenkircher <michaelsbordenkircher@gmail.com>
2018-03-06 19:06:04 -0500

    Added range validator to score.

--- CSharp2018Project/CSharp2018Project/Models/Drink.cs
+++ CSharp2018Project/CSharp2018Project/Models/Drink.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSharp2018Project.Models
 {
@@ -11,6 +12,7 @@ namespace CSharp2018Project.Models
        public int Id { get; set; }
        public string Name { get; set; }
        public string Category { get; set; }
+       [Range(0, 10, ErrorMessage = "Please enter a value between 0 and 10.")]
        public int? Score { get; set; }
     }
 
